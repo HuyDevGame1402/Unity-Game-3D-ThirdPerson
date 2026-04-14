15161b6bd0e277d0b0fe8090970f1cd1d14303c9
HuyDevGame1402 <huyco14022004@gmail.com>
2026-04-14 16:52:57 +0700

    Add jump gravity animation

--- Assets/Scirpts/PlayerScript.cs
+++ Assets/Scirpts/PlayerScript.cs
@@ -6,12 +6,15 @@ public class PlayerScript : MonoBehaviour
 {
     [Header("Player Movement")]
     public float playerSpeed = 1.9f;
+    public float playerSprint = 3.0f;
 
     [Header("Player Script Camera")]
     public Transform playerCamera;
 
     [Header("Player Animator and Gravity")]
     public CharacterController cC;
+    public float gravity = -9.81f;
+    public Animator animator;
 
     [Header("Input Value")]
     private float horizontal_axis;
@@ -24,13 +27,30 @@ public class PlayerScript : MonoBehaviour
     [Header("Player Jumping and velocity")]
     public float turnCalmTime = 0.1f;
     private float turnCalmVelocity;
+    public float jumpRange = 1.0f;
+    private Vector3 velocity;
+    public Transform surfaceCheck;
+    private bool onSurface;
+    public float surfaceDistance = 0.4f;
+    public LayerMask surfaceMask;
 
     private void Update()
     {
-        PlayerMove();
+        onSurface = Physics.CheckSphere(surfaceCheck.position, 
+            surfaceDistance, surfaceMask);
+        if (onSurface && velocity.y < 0)
+        {
+            velocity.y = -2f;
+        }
+        velocity.y += gravity * Time.deltaTime;
+        cC.Move(velocity * Time.deltaTime);
+
+        PlayerMove(playerSpeed, true);
+        Jump();
+        Sprint();
     }
 
-    private void PlayerMove()
+    private void PlayerMove(float speed, bool isWalk)
     {
         // lấy input từ bàn phím
         horizontal_axis = Input.GetAxis("Horizontal");
@@ -42,6 +62,15 @@ public class PlayerScript : MonoBehaviour
         // nếu có input
         if (direction.magnitude >= 0.1f)
         {
+            if (isWalk)
+            {
+                SetAnimWalk();
+            }
+            else
+            {
+                SetAnimSprint();
+            }
+
             // lấy góc quay của hg + thêm vào góc quay hiện tại của camera ra đc góc quay cần
             // ví dụ camera góc 90 ấn W => góc dir = 90 -> targetAngle = 90 + 90 = 180 => camera quay về hướng 180
             targetAngle = Mathf.Atan2(direction.x, direction.z) * Mathf.Rad2Deg + playerCamera.eulerAngles.y;
@@ -51,7 +80,73 @@ public class PlayerScript : MonoBehaviour
             transform.rotation = Quaternion.Euler(0, angle, 0);
             // chuyển hướng forward về hướng targetAngle để di chuyển player (hướng z thẳng )
             moveDirection = Quaternion.Euler(0, targetAngle, 0) * Vector3.forward;
-            cC.Move(moveDirection.normalized * playerSpeed * Time.deltaTime);
+            cC.Move(moveDirection.normalized * speed * Time.deltaTime);
         }
+        else
+        {
+            if (isWalk)
+            {
+                SetAnimIdle();
+            }
+            else
+            {
+                SetAnimIdleInSprint();
+            }
+        }
+    }
+    private void Jump()
+    {
+        if (Input.GetButtonDown("Jump") && onSurface)
+        {
+            SetAnimJump();
+            velocity.y = Mathf.Sqrt(jumpRange * -2f * gravity);
+        }
+        else
+        {
+            ResetAnimJump();
+        }
+    }
+
+    private void Sprint()
+    {
+        if(Input.GetKey(KeyCode.LeftShift) && Input.GetKey(KeyCode.W) ||
+            Input.GetKey(KeyCode.UpArrow) && onSurface)
+        {
+            PlayerMove(playerSprint, false);
+        }
+    }
+    private void SetAnimWalk()
+    {
+        animator.SetBool("Idle", false);
+        animator.SetBool("Walk", true);
+        animator.SetBool("Running", false);
+        animator.SetBool("RifleWalk", false);
+        animator.SetBool("IdleAim", false);
+    }
+    private void SetAnimIdle()
+    {
+        animator.SetBool("Idle", true);
+        animator.SetBool("Walk", false);
+        animator.SetBool("Running", false);
+    }
+    private void SetAnimIdleInSprint()
+    {
+        animator.SetBool("Walk", true);
+        animator.SetBool("Running", false);
+    }
+    private void SetAnimSprint()
+    {
+        animator.SetBool("Walk", false);
+        animator.SetBool("Running", true);
+    }
+    private void SetAnimJump()
+    {
+        animator.SetBool("Idle", false);
+        animator.SetTrigger("Jump");
+    }
+    private void ResetAnimJump()
+    {
+        animator.SetBool("Idle", true);
+        animator.ResetTrigger("Jump");
     }
 }
